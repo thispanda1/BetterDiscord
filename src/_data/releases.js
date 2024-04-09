69ee7bf06921bc64ae09bf7a325e844e4dff15c4
thispanda1 <165739041+thispanda1@users.noreply.github.com>
2024-04-09 08:26:30 -0700

    Delete src directory

--- src/_data/releases.js
+++
@@ -1,8 +0,0 @@
-const Cache = require('@11ty/eleventy-cache-assets')
-
-module.exports = async function () {
-  return Cache('https://api.github.com/repos/ravynsoft/ravynos/releases', {
-    duration: '1h',
-    type: 'json',
-  })
-}
